8995d130b09e2cf6e65df28371f9e53d02ff732f
RenderKar <ehsanrezaee@hotmail.com>
2024-05-13 15:05:20 +0330

    fixing typescript

--- src/context/Shoppingcartcontext.tsx
+++ src/context/Shoppingcartcontext.tsx
@@ -1,47 +1,46 @@
 import { createContext, useContext, useState } from "react";
 
-export const shoppingcartcontext = createContext({} as shoppingcartcontext)
+export const Shoppingcartcontext = createContext({} as shoppingcartcontext);
 interface cartItem {
-    id: number,
-    qty: number
+  id: number;
+  qty: number;
 }
 interface shoppingcartcontext {
-    cartItems: cartItem[];
-    handleincreaseproductqty: (id: number) => void
+  cartItems: cartItem[];
+  handleincreaseproductqty: (id: number) => void;
 }
 
 interface shoppingcartprovider {
-    Children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export const useshoppingcartcontext = () => {
-    return useContext(shoppingcartcontext)
-}
+  return useContext(Shoppingcartcontext);
+};
 
-export function Shoppingcartprovider({ Children }: shoppingcartprovider) {
-    const [cartItems, SetcartItems] = useState<cartItem[]>([])
-    const handleincreaseproductqty = (id: number) => {
-        SetcartItems((currentItems) => {
-            let selectedItem = currentItems.find(item => item.id == id)
-            if (selectedItem == null) {
-                return [...currentItems, { id: id, qty: 1 }]
-            }
-            else {
-                currentItems.map(item => {
-                    if (item.id == id) {
-                        return { ...item, qty: item.qty + 1 }
-                    }
-                    else {
-                        return item
-                    }
-                }
-                )
-            }
-        })
-    }
-    return (
-        <shoppingcartcontext.Provider value={{ cartItems }}>
-            {Children}
-        </shoppingcartcontext.Provider>
-    )
+export function Shoppingcartprovider({ children }: shoppingcartprovider) {
+  const [cartItems, SetcartItems] = useState<cartItem[]>([]);
+  const handleincreaseproductqty = (id: number) => {
+    SetcartItems((currentItems) => {
+      let selectedItem = currentItems.find((item) => item.id == id);
+      if (selectedItem == null) {
+        return [...currentItems, { id: id, qty: 1 }];
+      } else {
+        currentItems.map((item) => {
+          if (item.id == id) {
+            return { ...item, qty: item.qty + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
+  return (
+    <Shoppingcartcontext.Provider
+      value={{ cartItems, handleincreaseproductqty }}
+    >
+      {children}
+    </Shoppingcartcontext.Provider>
+  );
 }
